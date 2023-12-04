89274c64f47868903cfc505213999d47ea88d308
patrick-codes <khofilyndon2018@gmail.com>
2023-12-04 11:37:48 +0000

    commit2

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\KofikTech\\Desktop\\Projects Hub New\\Pensa Project\\pensa_aamusted" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\KofikTech\\Desktop\\Projects Hub New\\Pensa Project\\pensa_aamusted"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\KofikTech\\Desktop\\Projects Hub New\\Pensa Project\\pensa_aamusted\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\KofikTech\\Desktop\\Projects Hub New\\Pensa Project\\pensa_aamusted"
+  "FLUTTER_TARGET=C:\\Users\\KofikTech\\Desktop\\Projects Hub New\\Pensa Project\\pensa_aamusted\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jZGJlZGE3ODhhMjkzZmEyOTY2NWRjM2ZhM2Q2ZTYzYmQyMjFjYjBkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\KofikTech\\Desktop\\Projects Hub New\\Pensa Project\\pensa_aamusted\\.dart_tool\\package_config.json"
+)
